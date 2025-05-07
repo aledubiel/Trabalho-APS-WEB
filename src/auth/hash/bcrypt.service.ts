92d35c5686ff26389a42fc981ecc86c7a9dcd20f
aledubiel <126979581+joaogson@users.noreply.github.com>
2025-05-06 22:03:05 -0300

    Commit 06.05 - Auth

---
+++ src/auth/hash/bcrypt.service.ts
@@ -0,0 +1,10 @@
+import { HashingServiceProtocol } from "./hashing.service";
+
+export class BcryptService extends HashingServiceProtocol{
+    async hash(password: string) : Promise<string> {
+        return ""
+    }
+    async compare(password: string, passwordHash: string): Promise<boolean> {
+        return true
+    }
+}
